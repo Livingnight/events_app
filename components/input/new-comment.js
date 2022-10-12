a29038981d02ddabfd79481b5c9bc69ff8a1537a
Jay Jernigan <jjernigan1065@gmail.com>
2022-10-12 15:47:23 -0400

    added name ref to new comment component

--- components/input/new-comment.js
+++ components/input/new-comment.js
@@ -5,19 +5,23 @@ export default function NewComment() {
 
   const [isInvalid, setIsInvalid] = useState(false)
 
+  const nameInputRef = useref()
   const emailInputRef = useRef()
   const commentInputRef = useRef()
-  
+
   function submitHandler(event) {
     event.preventDefault()
 
     const enteredEmail = emailInputRef.current.value
+    const enteredName = nameInputRef.current.value
     const enteredComment = commentInputRef.current.valu
 
     if (
       !enteredEmail ||
       enteredEmail.trim() === '' ||
       !enteredEmail.includes('@') ||
+      !enteredName ||
+      enteredName.trim() === '' ||
       !enteredComment ||
       enteredComment.trim() === ''
     ) {
@@ -27,6 +31,7 @@ export default function NewComment() {
 
     props.onAddComment({
       email: enteredEmail,
+      name: enteredName,
       comment: enteredComment
     })
   }
@@ -34,19 +39,19 @@ export default function NewComment() {
     <form className={classes.form}>
       <div className={classes.row}>
         <div className={classes.control}>
-          <label htmlFor='email'>Your Name</label>
+          <label htmlFor='email'>Your Email</label>
           <input type="email" ref={emailInputRef} id='email' />
         </div>
         <div className={classes.control}>
           <label htmlFor='name'>Your Name</label>
-          <input type="text" id='name' />
+          <input type="text" id='name' ref={nameInputRef}/>
         </div>
         <div className={classes.control}>
-          <label htmlFor='comment'>Your Name</label>
+          <label htmlFor='comment'>Your Comment</label>
           <textarea id='name' rows='5' ref={commentInputRef}></textarea>
         </div>
         {isInvalid && <p>Please enter a valid email address and comment!</p>}
-        <button>Submit</button>
+        <button className={classes.button}>Submit</button>
       </div>
     </form>
   )
